c9ecb496e4bd21ae06f40c7ffd781790b9feca1b
slowhand0309 <slowhand0309@gmail.com>
2016-05-06 18:01:47 +0900

    Update console_printer

--- include/printer.h
+++ include/printer.h
@@ -4,7 +4,17 @@
 #ifndef PRINTER_H_
 #define PRINTER_H_
 
+#include "mrlogger.h"
+#include <string>
+#include <ctime>
+
 namespace mrlogger {
+
+#define LOG_LEVEL_UNKOWN_STR "<?>" // Unkown symbol.
+#define LOG_LEVEL_INFO_STR   "<I>" // Infomation symbol.
+#define LOG_LEVEL_WARN_STR   "<W>" // Warning symbol.
+#define LOG_LEVEL_ERROR_STR  "<E>" // Error symbol.
+#define LOG_LEVEL_DEBUG_STR  "<D>" // Debug symbol.
 
 // Class printer.
 class Printer
@@ -20,6 +30,41 @@ public:
 
   // Print to write log.
 	virtual void print(int level, const char* message, va_list args) = 0;
+
+protected:
+
+	std::string toLevelString(unsigned int level) {
+		std::string str = LOG_LEVEL_UNKOWN_STR;
+		switch (level) {
+			case LOG_LEVEL_INFO:
+				str = LOG_LEVEL_INFO_STR;
+				break;
+			case LOG_LEVEL_WARN:
+				str = LOG_LEVEL_WARN_STR;
+				break;
+			case LOG_LEVEL_ERROR:
+				str = LOG_LEVEL_ERROR_STR;
+				break;
+			case LOG_LEVEL_DEBUG:
+				str = LOG_LEVEL_DEBUG_STR;
+				break;
+			default:
+				break;
+		}
+		return str;
+	}
+
+	// Get current date time string.
+	std::string getDateTime(const char *format) {
+
+		std::time_t t = std::time(NULL);
+		char str[128] = {0};
+
+		if (std::strftime(str, sizeof(str), format, std::localtime(&t))) {
+			return std::string(str);
+		}
+		return std::string("");
+	}
 };
 
 } // namespace mrlogger
--- lib/printer/console_printer.cpp
+++ lib/printer/console_printer.cpp
@@ -5,14 +5,27 @@
 
 namespace mrlogger {
 
+// Date time format e.g. 2016/05/06 10:10:30
+#define DATE_TIME_FORMAT    "%Y/%m/%d %H:%M:%S"
+
+// Constructor.
 ConsolePrinter::ConsolePrinter()
   : Printer() {
 }
 
+// Destructor.
 ConsolePrinter::~ConsolePrinter() {
 }
 
+// Print log to console.
 void ConsolePrinter::print(int level, const char* message, va_list args) {
+
+  // Get current date time.
+  std::string date_time = getDateTime(DATE_TIME_FORMAT);
+  // Get log level string.
+  std::string log_level = toLevelString(level);
+
+  printf("%s %s ", log_level.c_str(), date_time.c_str());
 
   // TODO for debug.
   vprintf(message, args);
