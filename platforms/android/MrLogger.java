1673423e65c0dad56abbe38f18eadb37361d8126
slowhand0309 <slowhand0309@gmail.com>
2016-06-01 23:07:01 +0900

    Added android platform

---
+++ platforms/android/MrLogger.java
@@ -0,0 +1,11 @@
+
+
+class MrLogger {
+
+  native void info(String message);
+  native void warn(String message);
+  native void error(String message);
+  native void debug(String message);
+
+  // TODO implemention.
+}
